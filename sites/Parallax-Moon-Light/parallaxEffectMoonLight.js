304d26ebafe5558a9ab95e069cd91be40a07b601
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-10-28 23:24:52 +0300

    Update layout and add media in second site

--- sites/Parallax-Moon-Light/parallaxEffectMoonLight.js
+++ sites/Parallax-Moon-Light/parallaxEffectMoonLight.js
@@ -6,23 +6,6 @@ let moon = document.getElementById("moon");
 let mountain = document.getElementById("mountain");
 let road = document.getElementById("road");
 let text = document.getElementById("text");
-let tittleFooter = document.getElementById("footer-tittle");
-let gitimgLink = document.getElementById("gitImg");
-let isTFvis = false;
-
-window.addEventListener('scroll', function() {
-    tittleFooter.style.transition = '3.5s';
-    tittleFooter.style.transform = 'translateX(-0%)';
-    isTFvis = true;
-    function visGitImg() {
-        gitimgLink.style.transition = '2.5s';
-        gitimgLink.style.visibility = 'visible';
-        gitimgLink.style.transform = 'translateX(240%)';
-    } 
-    if(isTFvis == true) {
-        setTimeout(visGitImg,3500);
-    }
-});
 
 window.addEventListener('scroll',function() {
     let value = window.scrollY;
@@ -31,4 +14,4 @@ window.addEventListener('scroll',function() {
     mountain.style.top = value * 0.15 + 'px';
     road.style.top = value * 0.15 + 'px';
     text.style.top = value + 'px';
-});
+})
